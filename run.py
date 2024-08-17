13db9b55019408e96daf161420c10485a97fde84
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-08-17 11:16:13 +0000

    Add remaining questions of survey

--- run.py
+++ run.py
@@ -14,18 +14,34 @@ SHEET = GSPREAD_CLIENT.open('survey_results')
 
 print('Thank you for visiting Bunratty Castle today.')
 print('We value your opinion and would love to hear your thoughts!')
-print('Could you please spare a few minutes to complete this survey?')
+print('Could you please spare a few minutes to complete this survey?\n')
 
-def survey_feedback_first_question():
+def survey_feedback():
     """
-    Get answer to the first survey question from the user
+    Get answer survey questions from the user
     """
     print('How would you rate your overall experience at Bunratty Castle today?')
-    print('1 = Poor, 5 = Excellent')
+    print('1 = Poor, 5 = Excellent\n')
 
     answer_one = input("Please enter your answer here: ")
 
-    return answer_one
+    print('\nHow would you rate the quality of customer service provided by the staff?')
+    print('1 = Very Poor, 5 = Excellent\n')
 
-response_one = survey_feedback_first_question()
+    answer_two = input("Please enter your answer here: ")
+
+    print('\nHow likely are you to recommend this attraction to a friend or family member?')
+    print('1 = Not Likely, 5 = Very Likely\n')
+
+    answer_three = input("Please enter your answer here: ")
+
+    return answer_one, answer_two, answer_three
+
+responses = survey_feedback()
+
+print(responses)
+
+
+    
+
 

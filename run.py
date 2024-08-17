7623246f7a7a7f3251d95a0598297ba9dc1581b8
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-08-17 10:37:19 +0000

    Create introduction and first survey question

--- run.py
+++ run.py
@@ -12,8 +12,20 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('survey_results')
 
-results = SHEET.worksheet('results')
+print('Thank you for visiting Bunratty Castle today.')
+print('We value your opinion and would love to hear your thoughts!')
+print('Could you please spare a few minutes to complete this survey?')
 
-data = results.get_all_values()
+def survey_feedback_first_question():
+    """
+    Get answer to the first survey question from the user
+    """
+    print('How would you rate your overall experience at Bunratty Castle today?')
+    print('1 = Poor, 5 = Excellent')
 
-print(data)
+    answer_one = input("Please enter your answer here: ")
+
+    return answer_one
+
+response_one = survey_feedback_first_question()
+

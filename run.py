677a630fda24b8e025e4bb5c8e7e8c6f78c0b66b
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-08-19 19:31:57 +0000

    Create function for welcome message. Change how input in validated so it validates after each question instead of at the end

--- run.py
+++ run.py
@@ -12,58 +12,62 @@ SCOPED_CREDS = CREDS.with_scopes(SCOPE)
 GSPREAD_CLIENT = gspread.authorize(SCOPED_CREDS)
 SHEET = GSPREAD_CLIENT.open('survey_results')
 
-print('Thank you for visiting Bunratty Castle today.')
-print('We value your opinion and would love to hear your thoughts!')
-print('Could you please spare a few minutes to complete this survey?\n')
 
 def survey_feedback():
     """
     Get answer survey questions from the user
     """
-    print('How would you rate your overall experience at Bunratty Castle today?')
+    print('How would you rate your overall experience at Bunratty Castle?')
     print('1 = Very Poor, 2 = Poor, 3 = Neutral, 4 = Good, 5 = Excellent\n')
 
-    answer_one = input("Please enter your answer here: ")
+    answer_one = input("Please enter your answer here: ").strip()
+    while answer_one == '' or is_input_not_valid(answer_one):
+        answer_one = input('Invalid input! Please enter a number between 1 and 5: ').strip()
 
-    print('\nHow would you rate the quality of customer service provided by the staff?')
+    print('\nHow would you rate the quality of customer service provided?')
     print('1 = Very Poor, 2 = Poor, 3 = Neutral, 4 = Good, 5 = Excellent\n')
 
-    answer_two = input("Please enter your answer here: ")
+    answer_two = input("Please enter your answer here: ").strip()
+    while answer_two == '' or is_input_not_valid(answer_two):
+        answer_two = input('Invalid input! Please enter a number between 1 and 5: ').strip()
 
-    print('\nHow likely are you to recommend this attraction to a friend or family member?')
+    print('\nHow likely are you to recommend this attraction to a friend?')
     print('1 = Very Unlikely, 2 = Unlikely, 3 = Neutral, 4 = Likely, 5 = Very Likely\n')
 
-    answer_three = input("Please enter your answer here: ")
+    answer_three = input("Please enter your answer here: ").strip()
+    while answer_three == '' or is_input_not_valid(answer_three):
+        answer_three = input('Invalid input! Please enter a number between 1 and 5: ').strip()
 
-    print('\nThank you for your response')
+    print('\nThank you for your response\n')
 
     return answer_one, answer_two, answer_three
 
 
-def validate_response(answer):
+def is_input_not_valid(answer):
     """
-    Check if input entered is a number between 1 and 5
+    This checks the answer to ensure it is between 1 and 5
+    and returns a Boolean True or False
     """
-    try:
-        number = int(answer)
-        if 1 <= number <= 5:
-            return number
-        else:
-            return None       
-    except ValueError:
-        return None
-        
-responses = survey_feedback()
+    RESPONSES = ['1', '2', '3', '4', '5']
+    if answer in RESPONSES:
+        return False
+    return True
 
-validated_responses = []
-for response in responses:
-    valid_response = validate_response(response)
-    if valid_response is None:
-        print("Invalid input. Please enter a valid number between 1 and 5")
-    else:
-        validated_responses.append(valid_response)
 
-print(f"Validated responses: {validated_responses}")
+def welcome_message():
+    """
+    Welcome message for the user before starting the survey
+    """
+    print('Thank you for visiting Bunratty Castle today.')
+    print('We value your opinion and would love to hear your thoughts!')
+    print('Could you please spare a few minutes to complete this survey?\n')
+
+
+if __name__ == '__main__':
+    welcome_message()
+    survey_feedback()
+
+
 
 
 

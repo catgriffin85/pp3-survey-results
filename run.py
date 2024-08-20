8da030d5e50acfd412b65fd7d6fd1804e6000d76
Catherine Griffin <catherine.griffin@students.codeinstitute.net>
2024-08-20 10:28:18 +0000

    Clean up code to remove whitespaces and shorten lines

--- run.py
+++ run.py
@@ -22,26 +22,26 @@ def survey_feedback():
 
     answer_one = input("Please enter your answer here: ").strip()
     while answer_one == '' or is_input_not_valid(answer_one):
-        answer_one = input('Invalid input! Please enter a number between 1 and 5: ').strip()
+        answer_one = input('Invalid input! Please try again: ').strip()
 
     print('\nHow would you rate the quality of customer service provided?')
     print('1 = Very Poor, 2 = Poor, 3 = Neutral, 4 = Good, 5 = Excellent\n')
 
     answer_two = input("Please enter your answer here: ").strip()
     while answer_two == '' or is_input_not_valid(answer_two):
-        answer_two = input('Invalid input! Please enter a number between 1 and 5: ').strip()
+        answer_two = input('Invalid input! Please try again: ').strip()
 
     print('\nHow likely are you to recommend this attraction to a friend?')
-    print('1 = Very Unlikely, 2 = Unlikely, 3 = Neutral, 4 = Likely, 5 = Very Likely\n')
+    print('1= Not at All, 2= Unlikely, 3= Maybe, 4= Likely, 5= Very Likely\n')
 
     answer_three = input("Please enter your answer here: ").strip()
     while answer_three == '' or is_input_not_valid(answer_three):
-        answer_three = input('Invalid input! Please enter a number between 1 and 5: ').strip()
+        answer_three = input('Invalid input! Please try again: ').strip()
 
     print('\nThank you for taking the time to complete our survey!\n')
 
     return int(answer_one), int(answer_two), int(answer_three)
- 
+
 
 def is_input_not_valid(answer):
     """
@@ -70,9 +70,10 @@ def all_survey_results():
     for row in results:
         print(row)
 
+
 def question_one_count():
     results = SHEET.worksheet('results').get_all_values()
-    first_question_responses = [int(row[0]) for row in results if row [0].isdigit()]
+    first_question_responses = [int(row[0]) for row in results if row[0].isdigit()]
 
     count_five = first_question_responses.count(5)
 
@@ -83,6 +84,7 @@ def question_one_count():
     print(f'Number of fives: {count_five}')
     print(f'Total responses: {total_responses}')
     print(f'Percentage of fives: {percentage_of_fives_q1}%')
+
 
 def welcome_message():
     """
@@ -104,16 +106,3 @@ if __name__ == '__main__':
     all_survey_results()
 
     question_one_count()
-
-
-    
-
-
-
-
-
-
-
-    
-
-
